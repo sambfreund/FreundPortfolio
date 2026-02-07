9a09e1a6d5aedbdd6e811273a0eaa1c81a740754
sambfreund <sambfreund@gmail.com>
2026-02-07 14:49:58 -0600

    Blog Update

--- js/script.js
+++ js/script.js
@@ -17,72 +17,18 @@ $(document).ready(function() {
 
  $('#slides').superslides({
  	animation: 'fade',
- 	play: 5000
+ 	play: 5000,
+ 	inherit_width_from: $('#hero-carousel'),
+ 	inherit_height_from: $('#hero-carousel')
  });
 
 	var typed = new Typed(".typed", {
- 		strings: ["Father.", "Quality Assurance Professional.", "Designer.", "Husband.", "Developer.", "Life Long Learner."],
+ 		strings: ["Senior Quality Engineer.", "Father.", "Designer.", "Husband.", "Developer.", "Life Long Learner."],
  		typeSpeed: 80,
  		loop: true,
  		startDelay: 1000,
  		showCursor: false
  	});
-
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	        	items:4
-	        }
-	    }
-	});
-
-
-		var skillsTopOffSet = $(".skillsSection").offset().top;
-		var statsTopOffSet = $(".statsSection").offset().top;
-		var countUpFinished = false;
-
-		$(window).scroll(function() {
-
-			if(window.pageYOffset > skillsTopOffSet - $(window).height() + 200) {
-
-				$('.chart').easyPieChart({
-		    		easing: 'easeInOut',
-		    		barColor: '#FFF',
-		    		trackColor: false,
-		    		scaleColor: false,
-		    		lineWidth: 4,
-		    		size: 152,
-		    		onStep: function(from, to, percent) {
-		    			$(this.el).find('.percent').text(Math.round(percent));
-		    		}
-				});
-
-			}
-
-
-			if(!countUpFinished && window.pageYOffset > statsTopOffSet - $(window).height() + 200) {
-				$(".counter").each(function() {
-					var element = $(this);
-					var endVal = parseInt(element.text());
-
-					element.countup(endVal);
-				})
-
-					countUpFinished = true;
-			}
-
-		});
 
 		$("[data-fancybox]").fancybox();
 
